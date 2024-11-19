0cd0f03b1737adec3db9785be7eeed04d3c0ee6d
TheodoreFORTE <theodore.forte@gmail.com>
2024-11-19 15:50:14 +0100

    First complete implementation

--- Assets/Scripts/DragDropSpawnHandler.cs
+++ Assets/Scripts/DragDropSpawnHandler.cs
@@ -1,51 +1,107 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.Experimental.AI;
+using Unity.VisualScripting;
 
 public class DragDropSpawnHandler : MonoBehaviour, DragDropHandler
 {
 
-    public bool CanBeDragged()
+    
+    // Through inspector
+    [SerializeField]
+    private GameEvent spawnPositionRequests;
+
+    [SerializeField]
+    private TileEventContainer moveRequest;
+
+
+
+
+    // On runtime
+    private Vector3 worldPosition;
+    private Vector3Int gridPosition;
+
+
+    //Instanciated on  Awake()
+
+    private List<Vector3Int> spawnPositions;
+    private Transform parentTransform;
+    private GameObject mainCanvas;
+
+
+    void Awake()
+    {
+        spawnPositions = new List<Vector3Int>();
+        spawnPositionRequests?.Invoke();
+        parentTransform = transform.parent.transform;
+        mainCanvas = GameObject.FindGameObjectWithTag("MainCanvas");        
+    }
+
+    void Update()
     {
         /*
-            Unless there is a different input required, the game will always let the player drag the champions
-       
+            Needs to check if all the champion Controller have been correctly affected to a Spawn Tile
         */
+    }
 
 
+    public void RegisterSpawnPosition(GameEventArgs<Vector3Int> spawnCommunication)
+    {
+        spawnPositions.Add(spawnCommunication.data);
+        Debug.Log($"Spawn tile registered at {spawnCommunication.data}");
+    }
 
 
+    public bool CanBeDragged()
+    {
         return true;
     }
 
     public bool CanBeDroppedHere()
     {
-        /*
-            Check the tile where the image is being dropped. If it's a spawn point : 
-            
+        /*           
+            Check the tile where the image is being dropped. If it's a spawn point return true:            
         */
-
-
+        worldPosition = transform.position;
+        gridPosition = TilemapLocator.Instance.WorldToGrid(worldPosition);
+        
+        
+        if(spawnPositions.Contains(gridPosition))
+        {
+            return true;
+        }
 
         return false;
     }
 
     public void OnRightDrop()
-    {
-        /*
-            Set its parent to the spawn point that will be created.
-            But right before, if there was already an image, put the image back to the Champion Launcher
-        */
+    {    
+        // Removing the gameObject from the Champion launcher 
+        parentTransform.SetParent(mainCanvas.transform);     
 
+        //Repositioning the draggable related to the controller
+        transform.localPosition = new Vector3(0,0,0);
 
-
+        //Creating a request
+        GameEventArgs<Vector3Int> requestArgs = new GameEventArgs<Vector3Int>(transform.parent.gameObject, gridPosition);
+             
+        //Invoking the request
+        moveRequest?.onEvent.Invoke(requestArgs);
+        
+        
         Debug.Log("Right drop !");
+        
+        /*            
+            ToBeImplemented -> the management of a drop on tile with a different champion already in
+            But right before, if there was already an image, put the image back to the Champion Launcher
+        */        
     }
 
     public void OnWrongDrop()
     {
         /*  
-            The element return where he was from
+            The element return where he was from by default
         */
 
 
