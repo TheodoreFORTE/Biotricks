708f9b46a67e7769532b68548c01421250886695
TheodoreFORTE <theodore.forte@gmail.com>
2024-11-19 15:51:07 +0100

    New Event container

---
+++ Assets/Scripts/ScriptableObjets/ChampionEventContainer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Champion Event Container", menuName = "Biotricks/GenericEventContainer/ChampionEventContainer")]
+public class ChampionEventContainer : GenericEventContainer<GameEventArgs<Champion>>
+{
+
+}
