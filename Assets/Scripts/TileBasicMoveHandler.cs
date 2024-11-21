79bfd92ade49f66a2b4fe42cd6909b8bbb4f291d
TheodoreFORTE <theodore.forte@gmail.com>
2024-11-21 09:39:36 +0100

    Part of the renaming policy

--- Assets/Scripts/TileBasicMoveHandler.cs
+++ Assets/Scripts/TileBasicMoveHandler.cs
@@ -1,7 +1,7 @@
 
 using UnityEngine;
 
-public class TileBasicMoveHandler : MonoBehaviour, DragDropHandler
+public class TileBasicMoveHandler : MonoBehaviour, IDragDropHandler
 {
     
     public bool CanBeDragged()
